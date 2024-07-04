63f2a40ad10831dd1f8e49c5018efe408b5c7014
Eltrone <amineelorche@gmail.com>
2024-07-04 15:46:02 +0000

    file added

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
+"""
+Module 0-lockboxes
+Contains function that determines if all boxes can be opened
+"""
+
 def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened
+
+    Parameters:
+    boxes (list of list of int): list of boxes with keys
+
+    Returns:
+    bool: True if all boxes can be opened, False otherwise
+    """
     if not boxes:
         return False
 
@@ -15,14 +29,3 @@ def canUnlockAll(boxes):
                     keys.append(new_key)
 
     return len(unlocked) == len(boxes)
-
-# Test cases
-if __name__ == "__main__":
-    boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))  # True
-
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))  # True
-
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  # False
