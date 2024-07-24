4e2e3dc4d524bd6b03ba300b8aae3e440d192f2b
Eltrone <amineelorche@gmail.com>
2024-07-24 23:30:37 +0000

    file modified

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,6 +3,7 @@
 Module to validate UTF-8 encoding.
 """
 
+
 def validUTF8(data):
     """
     Determine if a given data set represents a valid UTF-8 encoding.
@@ -13,14 +14,14 @@ def validUTF8(data):
     for byte in data:
         # Masque pour identifier les bits les plus significatifs de l'octet.
         mask = 0b10000000
-        
+
         if continuation_bytes == 0:
             # Compter le nombre de bits à 1 au début de l'octet.
             while byte & mask:
                 continuation_bytes += 1
                 mask >>= 1
 
-            # S'il n'y a pas de bits à 1, il s'agit d'un caractère ASCII sur un seul octet.
+            # Si pas de bits à 1, c'est un caractère ASCII sur un seul octet.
             if continuation_bytes == 0:
                 continue
 
@@ -30,12 +31,12 @@ def validUTF8(data):
 
             # Réduire de 1 pour exclure l'octet de démarrage.
             continuation_bytes -= 1
-        
+
         else:
             # Vérifier si l'octet est un octet de continuation.
             if not (byte & 0b10000000 and not (byte & 0b01000000)):
                 return False
-            
+
             continuation_bytes -= 1
 
     # S'il reste des octets de continuation attendus, la chaîne est tronquée.
