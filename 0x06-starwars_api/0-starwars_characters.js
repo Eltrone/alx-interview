64d72d06512ccb71370048493120d1ffcb3b34b1
Eltrone <amineelorche@gmail.com>
2024-08-09 00:03:27 +0000

    file modified

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,15 +1,38 @@
 #!/usr/bin/node
 const request = require('request');
 const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
 
-request(url, { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  let characters = body.characters;
-  characters.forEach(character => {
-    request(character, { json: true }, (err, res, body) => {
-      if (err) { return console.log(err); }
-      console.log(body.name);
+if (!movieId) {
+  console.log('Erreur: Utilisez ce format: ./0-starwars_characters.js <ID du film>');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+const fetchFromApi = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, { json: true }, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body);
+      }
     });
   });
-});
+};
+
+const displayCharacters = async () => {
+  try {
+    const filmDetails = await fetchFromApi(apiUrl);
+    const characterEndpoints = filmDetails.characters;
+
+    for (let endpoint of characterEndpoints) {
+      const characterDetails = await fetchFromApi(endpoint);
+      console.log(characterDetails.name);
+    }
+  } catch (error) {
+    console.error('Erreur lors de la récupération:', error);
+  }
+};
+
+displayCharacters();
