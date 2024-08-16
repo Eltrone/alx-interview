1df848e1c81676ea2cb2795a1f97a033ffdaf30e
Eltrone <amineelorche@gmail.com>
2024-08-16 01:21:28 +0000

    file added

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
 """
-Fonction rotate_2d_matrix pour pivoter matrice 90 degrés sens horaire.
+Fonction pour pivoter une matrice 2D de 90 degrés dans le sens horaire.
 """
 
 
 def rotate_2d_matrix(matrix):
     n = len(matrix)
 
-    # Transposer la matrice
+    # Transposition de la matrice
     for i in range(n):
         for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-    # Inverser chaque colonne
+    # Inversion des rangées pour la rotation horaire
     for i in range(n):
-        for j in range(n // 2):
-            matrix[j][i], matrix[n - j -
-                                 1][i] = matrix[n - j - 1][i], matrix[j][i]
+        matrix[i].reverse()
 
 
-# Test de la fonction
+# Test de la fonction avec une matrice 3x3
 if __name__ == "__main__":
     matrix = [[1, 2, 3],
               [4, 5, 6],
               [7, 8, 9]]
     rotate_2d_matrix(matrix)
-    print(matrix)  # Devrait afficher [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+    print(matrix)  # Attendu: [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
